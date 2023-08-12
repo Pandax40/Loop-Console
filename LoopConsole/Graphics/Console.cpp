6ebe8bb974d3f8d44a49d3396013a1d02636a310
Pandax40 <pandaxsantin@gmail.com>
2023-08-13 01:31:34 +0200

    Initial Screen Created

--- LoopConsole/Graphics/Console.cpp
+++ LoopConsole/Graphics/Console.cpp
@@ -1,6 +1,26 @@
 #include "Console.h"
 #include "../Const/Const.h"
 
+void Console::InitScreen()
+{
+	printf("\x1b[?25l"); //Cursor Hidding
+	printf("\x1b[%d;%dH", GLOBALS::WINDOW_SIZE_Y/45, GLOBALS::WINDOW_SIZE_X/26); //Initial Screen Position
+	printf("\x1b[s  _                      \x1b[u\x1b[1B");
+	printf("\x1b[s | |    ___   ___  _ __  \x1b[u\x1b[1B");
+	printf("\x1b[s | |   / _ \\ / _ \\| '_ \\ \x1b[u\x1b[1B");
+	printf("\x1b[s | |__| (_) | (_) | |_) |\x1b[u\x1b[1B");
+	printf("\x1b[s |_____\\___/ \\___/| .__/ \x1b[u\x1b[1B");
+	printf("\x1b[s                  |_|    \x1b[u\x1b[1B");
+	printf("\x1b[1B\x1b[6D"); //Go down 1 lines and 6 back chars
+	printf("\x1b[s   ____                      _      \x1b[u\x1b[1B");
+	printf("\x1b[s  / ___|___  _ __  ___  ___ | | ___ \x1b[u\x1b[1B");
+	printf("\x1b[s | |   / _ \\| '_ \\/ __|/ _ \\| |/ _ \\\x1b[u\x1b[1B");
+	printf("\x1b[s | |__| (_) | | | \\__ \\ (_) | |  __/\x1b[u\x1b[1B");
+	printf("\x1b[s  \\____\\___/|_| |_|___/\\___/|_|\\___|\x1b[u\x1b[1B");
+	printf("\x1b[3B\x1b[5C");
+	printf("PULSE ENTER PARA CONTINUAR");
+}
+
 void Console::DrawPiece(Piece piece, int baseX, int baseY)
 {	
 	switch (piece.GetType())
@@ -42,5 +62,96 @@ void Console::DrawPiece(Piece piece, int baseX, int baseY)
 		break;
 	default:
 		break;
+	}
+}
+
+void Console::FixUnions(int x, int y, Piece (*matrix)[GLOBALS::ROW_MATRIX][GLOBALS::COLUMN_MATRIX])
+{
+	if ((*matrix)[y][x].CheckFace(Piece::Top))
+	{
+		if (y == 0 or !(*matrix)[y - 1][x].CheckFace(Piece::Bottom))
+			printf("\x1b[%d;%dH_", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y), (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 5);
+		else if ((*matrix)[y - 1][x].CheckFace(Piece::Bottom))
+			printf("\x1b[%d;%dH ", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y), (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 5);
+	}
+	else if (y != 0)
+	{
+		if ((*matrix)[y - 1][x].CheckFace(Piece::Bottom))
+			printf("\x1b[%d;%dH_", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y), (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 5);
+		else
+			printf("\x1b[%d;%dH ", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y), (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 5);
+	}
+	else
+		printf("\x1b[%d;%dH ", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y), (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 5);
+
+	if ((*matrix)[y][x].CheckFace(Piece::Right))
+	{
+		if (x == GLOBALS::COLUMN_MATRIX - 1 or !(*matrix)[y][x + 1].CheckFace(Piece::Left))
+		{
+			printf("\x1b[%d;%dH  ", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 2, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 9);
+			printf("\x1b[%d;%dH│ ", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 3, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 9);
+		}
+		else if ((*matrix)[y][x + 1].CheckFace(Piece::Left))
+		{
+			printf("\x1b[%d;%dH__", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 2, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 9);
+			printf("\x1b[%d;%dH__", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 3, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 9);
+		}
+
+	}
+	else if (x != GLOBALS::COLUMN_MATRIX - 1)
+	{
+		if ((*matrix)[y][x + 1].CheckFace(Piece::Left))
+		{
+			printf("\x1b[%d;%dH  ", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 2, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 9);
+			printf("\x1b[%d;%dH │", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 3, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 9);
+		}
+		else
+		{
+			printf("\x1b[%d;%dH  ", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 2, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 9);
+			printf("\x1b[%d;%dH  ", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 3, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 9);
+		}
+	}
+
+	if ((*matrix)[y][x].CheckFace(Piece::Bottom))
+	{
+		if (y == GLOBALS::ROW_MATRIX - 1 or !(*matrix)[y + 1][x].CheckFace(Piece::Top))
+			printf("\x1b[%d;%dH_", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 5, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 5);
+		else if ((*matrix)[y + 1][x].CheckFace(Piece::Top))
+			printf("\x1b[%d;%dH ", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 5, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 5);
+	}
+	else if (y != GLOBALS::ROW_MATRIX - 1)
+	{
+		if ((*matrix)[y + 1][x].CheckFace(Piece::Top))
+			printf("\x1b[%d;%dH_", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 5, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 5);
+		else
+			printf("\x1b[%d;%dH ", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 5, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X) + 5);
+	}
+
+	if ((*matrix)[y][x].CheckFace(Piece::Left))
+	{
+		if (x == 0 or !(*matrix)[y][x - 1].CheckFace(Piece::Right))
+		{
+			printf("\x1b[%d;%dH  ", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 2, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X));
+			printf("\x1b[%d;%dH │", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 3, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X));
+		}
+		else if ((*matrix)[y][x - 1].CheckFace(Piece::Right))
+		{
+			printf("\x1b[%d;%dH__", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 2, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X));
+			printf("\x1b[%d;%dH__", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 3, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X));
+		}
+
+	}
+	else if (x != 0)
+	{
+		if ((*matrix)[y][x - 1].CheckFace(Piece::Right))
+		{
+			printf("\x1b[%d;%dH  ", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 2, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X));
+			printf("\x1b[%d;%dH│ ", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 3, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X));
+		}
+		else
+		{
+			printf("\x1b[%d;%dH  ", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 2, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X));
+			printf("\x1b[%d;%dH  ", (y * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y) + 3, (x * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X));
+		}
 	}
 }
--- LoopConsole/Graphics/Console.h
+++ LoopConsole/Graphics/Console.h
@@ -1,8 +1,11 @@
 #pragma once
 #include "../Tablero/Piece.h"
+#include "../Const/Const.h"
 
 namespace Console
 {
 	void DrawPiece(Piece piece, int baseX, int baseY);
+	void FixUnions(int x, int y, Piece (*matrix)[GLOBALS::ROW_MATRIX][GLOBALS::COLUMN_MATRIX]);
+	void InitScreen();
 }
 
