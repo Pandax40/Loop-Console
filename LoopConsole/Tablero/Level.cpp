126696a158ec7ec808f45f2d4717e28ac960b2fa
Pandax40 <pandaxsantin@gmail.com>
2023-08-13 01:33:14 +0200

    Levels finish indicator and Fix Unions in Level class

--- LoopConsole/Tablero/Level.cpp
+++ LoopConsole/Tablero/Level.cpp
@@ -1,6 +1,12 @@
 #include "Level.h"
 #include "../Const/Levels.h"
 #include "../Utility/Random.h"
+#include "../Graphics/Console.h"
+
+Level::Level()
+{
+
+}
 
 Level::Level(Piece (*matrix)[GLOBALS::ROW_MATRIX][GLOBALS::COLUMN_MATRIX])
 {
@@ -20,6 +26,9 @@ void Level::LoadLevel()
 			Console::DrawPiece((*matrix)[i][j], j * GLOBALS::FIGURE_SIZE_X + GLOBALS::MATRIX_BASE_X, i * GLOBALS::FIGURE_SIZE_Y + GLOBALS::MATRIX_BASE_Y);
 		}
 	}
+	for (int i = 0; i < GLOBALS::ROW_MATRIX; ++i)
+		for (int j = 0; j < GLOBALS::COLUMN_MATRIX; ++j)
+			Console::FixUnions(j, i, matrix);
 }
 
 
@@ -32,18 +41,22 @@ bool Level::CheckLevel()
 			Piece::Type type = (*matrix)[i][j].GetType();
 			if ((*matrix)[i][j].CheckFace(Piece::Top) and (i == 0 or !(*matrix)[i - 1][j].CheckFace(Piece::Bottom))) return false;
 			else if ((*matrix)[i][j].CheckFace(Piece::Right) and (j == GLOBALS::COLUMN_MATRIX - 1 or !(*matrix)[i][j + 1].CheckFace(Piece::Left))) return false;
-			else if ((*matrix)[i][j].CheckFace(Piece::Bottom) and (i == GLOBALS::ROW_MATRIX - 1 or !(*matrix)[i - 1][j].CheckFace(Piece::Top))) return false;
+			else if ((*matrix)[i][j].CheckFace(Piece::Bottom) and (i == GLOBALS::ROW_MATRIX - 1 or !(*matrix)[i + 1][j].CheckFace(Piece::Top))) return false;
 			else if ((*matrix)[i][j].CheckFace(Piece::Left) and (j == 0 or !(*matrix)[i][j - 1].CheckFace(Piece::Right))) return false;
-			else return true;
 		}
 	}
+	return true;
 }
 
 bool Level::NextLevel()
 {
 	++currentLevel;
-	if (currentLevel < sizeof(currentLevel) / sizeof(void*))
+	if (currentLevel < sizeof(Levels::Levels) / sizeof(void*))
 		LoadLevel();
-	else return false;
+	else
+	{
+		LEVELS_FINISHED = true;
+		return false;
+	}
 	return true;
 }
--- LoopConsole/Tablero/Level.h
+++ LoopConsole/Tablero/Level.h
@@ -1,13 +1,17 @@
 #pragma once
-#include "PieceMatrix.h"
+#include "Piece.h"
 #include "..\Const\Const.h"
 
 class Level
 {
 public:
+	Level();
 	Level(Piece (*matrix)[GLOBALS::ROW_MATRIX][GLOBALS::COLUMN_MATRIX]);
 	bool CheckLevel();
 	bool NextLevel();
+
+	static inline bool LEVELS_FINISHED = false;
+
 private:
 	int currentLevel;
 	void LoadLevel();
